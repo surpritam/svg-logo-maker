208c0e92b41bc0a1307c817b266b13ee13cc2f00
Pritam Sur <sur.pritam9878@gmail.com>
2024-06-23 17:46:41 -0400

    update project structure and add shapes class

--- generateLogo.js
+++ generateLogo.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const fs= require('fs');
+const fs = require('fs');
 
 inquirer
     .prompt([
@@ -32,5 +32,26 @@ inquirer
         },
     ])
     .then((data) => {
-        const {text, textColor, shape, shapeColor} = data;
-    })
+        const { text, textColor, shape, shapeColor } = data;
+
+        let shapeElement;
+        switch (shape) {
+            case 'circle':
+                shapeElement = `<circle cx="150" cy="100" r="50" fill="${shapeColor}" />`;
+                break;
+            case 'triangle':
+                shapeElement = `<polygon points="150,50 100,150 200,150" fill="${shapeColor}" />`;
+                break;
+            case 'square':
+                shapeElement = `<rect x="100" y="50" width="100" height="100" fill="${shapeColor}" />`;
+                break;
+        }
+        const svgContent = `
+<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+  ${shapeElement}
+  <text x="150" y="125" font-size="40" text-anchor="middle" fill="${textColor}">${text}</text>
+</svg>`;
+
+        fs.writeFileSync('logo.svg', svgContent.trim());
+        console.log('Generated logo.svg');
+    });
